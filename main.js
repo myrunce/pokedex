8feb5647441eddfb56d6921709c2688117e97120
Myron Navarrete <myronnavarrete@gmail.com>
2017-06-30 08:34:14 -0700

    Changed js and html

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+var pokemonArr =[];
+
 $(document).ready(function(){
     for(var i = 1; i <= 151; i++){
-        $('.pokemon').append('<img src="http://pokeapi.co/media/img/' + i + '.png">');
-    }
-});
+        $('.pokemon').append('<img id =\'' + i + '\' src="http://pokeapi.co/media/img/' + i + '.png">');
+        pokemonArr.push('http://pokeapi.co/media/img/' + i + '.png')
+    };
+    $(document).on("click", "img", function(){
+        $(".pokedex").append('<img src="' + pokemonArr[($(this).attr("id")) - 1] + '">')
+    })
+
+})
